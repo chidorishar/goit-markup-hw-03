4689b51cb51c1f7a7af182752542be2400abb508
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-04-21 21:15:59 +0300

    Added JS for portfolio buttons functionality & made some renaming for classes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function OnFilterClick(caller) {
+    var selectedCards = document.querySelectorAll(".card");
+    
+    selectedCards.forEach(function (card) {
+        card.classList.remove("hidden");
+    });
+    if (caller.id != "all-filter-button") {        
+        selectedCards = document.querySelectorAll(".card:not(."+ caller.id.replace("-filter-button", "-card")+")");        
+        selectedCards.forEach(function(card) {
+            card.classList.add("hidden")
+            });
+    }
+};
