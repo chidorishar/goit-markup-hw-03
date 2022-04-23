8278cc3cdd01b63f0395ee3db603f31ea231ce42
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-04-23 16:06:23 +0300

    Prettier formatting & more flexible card set styling

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 function OnFilterClick(caller) {
-    var selectedCards = document.querySelectorAll(".card");
-    
+  var selectedCards = document.querySelectorAll('.card');
+
+  selectedCards.forEach(function (card) {
+    card.classList.remove('hidden');
+  });
+  if (caller.id != 'all-filter-button') {
+    selectedCards = document.querySelectorAll(
+      '.card:not(.' + caller.id.replace('-filter-button', '-card') + ')'
+    );
     selectedCards.forEach(function (card) {
-        card.classList.remove("hidden");
+      card.classList.add('hidden');
     });
-    if (caller.id != "all-filter-button") {        
-        selectedCards = document.querySelectorAll(".card:not(."+ caller.id.replace("-filter-button", "-card")+")");        
-        selectedCards.forEach(function(card) {
-            card.classList.add("hidden")
-            });
-    }
-};
+  }
+}
